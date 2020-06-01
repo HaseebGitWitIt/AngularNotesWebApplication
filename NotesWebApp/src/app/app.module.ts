cfe018e906ef2a07d8e77642baab486dddd299f0
Haseeb <haseeb01001@gmail.com>
2020-06-01 12:00:48 -0400

    Trying to figure out

--- NotesWebApp/src/app/app.module.ts
+++ NotesWebApp/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
+
+
 @NgModule({
   declarations: [
     AppComponent
