3083adffc01704843cfc43de7e99d04afc439482
Haseeb <haseeb01001@gmail.com>
2020-06-04 08:46:22 -0400

    Configurations

--- NotesWebApp/src/app/app.module.ts
+++ NotesWebApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
